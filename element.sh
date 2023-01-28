44838b6e8cb5a3668826dabf876f15ee5c6b20d1
reinhardbuyabo <buyabor@gmail.com>
2023-01-28 09:40:56 +0000

    chore: Complete

--- element.sh
+++ element.sh
@@ -12,12 +12,18 @@ else
   else
     FETCH_ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol='$1' OR name='$1'")
   fi
-  echo $FETCH_ELEMENT | while IFS='|' read ATOMIC_NUMBER SYMBOL NAME
-  do
-    PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius,type FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
-    echo "$PROPERTIES" | while IFS='|' read ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+
+  if [[ ! $FETCH_ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo $FETCH_ELEMENT | while IFS='|' read ATOMIC_NUMBER SYMBOL NAME
     do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius,type FROM properties JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+      echo "$PROPERTIES" | while IFS='|' read ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+      do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      done
     done
-  done
+  fi
 fi
