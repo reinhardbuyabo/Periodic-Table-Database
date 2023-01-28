818a6fb1d8fbee6645ad9657079c906cd92a6a1b
reinhardbuyabo <buyabor@gmail.com>
2023-01-28 08:21:57 +0000

    feat: fetching elements

--- element.sh
+++ element.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
 if [[ ! $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  echo "hello world"
+  FETCH_ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1 OR symbol='$1' OR name='$1'")
 fi
